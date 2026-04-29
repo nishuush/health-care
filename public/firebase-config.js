9baf127142e0a72794c468f6b30dbb449c442ef2
Hemant <106817003+hemantHQ@users.noreply.github.com>
2026-04-29 10:53:47 +0530

    Update firebase-config.js

--- public/firebase-config.js
+++ public/firebase-config.js
@@ -5,5 +5,5 @@ window.FIREBASE_CONFIG = {
   storageBucket: "health-care-20f00.firebasestorage.app",
   messagingSenderId: "675402620572",
   appId: "1:675402620572:web:bebcdabf0a99fbe0445759",
-  measurementId: "G-0R2SQV6PNZ"
+  measurementId: "G-0R2SQV6PNZ",
 };

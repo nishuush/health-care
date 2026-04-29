2ed4aa87795f07d40d8a321d3d918f7734b984c3
Hemant <106817003+hemantHQ@users.noreply.github.com>
2026-04-29 10:50:35 +0530

    Google

--- public/firebase-config.js
+++ public/firebase-config.js
@@ -1,9 +1,9 @@
 window.FIREBASE_CONFIG = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
-  measurementId: "",
+  apiKey: "AIzaSyAA5P9KMrtzsO7vUDpUeZZY7cCWm2rCxjY",
+  authDomain: "health-care-20f00.firebaseapp.com",
+  projectId: "health-care-20f00",
+  storageBucket: "health-care-20f00.firebasestorage.app",
+  messagingSenderId: "675402620572",
+  appId: "1:675402620572:web:bebcdabf0a99fbe0445759",
+  measurementId: "G-0R2SQV6PNZ"
 };
